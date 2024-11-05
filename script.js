6be5efad0ca4d009594ee6217e38295547db910a
kenshi916 <taesaechao22@gmail.com>
2024-11-04 23:07:39 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
 
-// Show the ammo list when the button is clicked
-function showAmmo() {
-    const ammoList = document.getElementById("ammo-list");
-    if (ammoList.style.display === "none") {
-        ammoList.style.display = "block";
-    } else {
-        ammoList.style.display = "none";
-    }
-}
